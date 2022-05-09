53a315dbf5d1a04852b5ff75acb50364fcd2de30
parsa <parsamesg@gmail.com>
2022-05-09 22:12:26 +0100

    init new neovim config in lua

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,30 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use "wbthomason/packer.nvim"
+  -- colorschemes
+  use "morhetz/gruvbox"
+  use "Mofiqul/vscode.nvim"
+
+  -- appearance
+  use "nvim-lualine/lualine.nvim"
+  use "kyazdani42/nvim-web-devicons"
+
+  -- utils
+  use 'moll/vim-bbye'
+end)
