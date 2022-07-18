88ff58de44973c0fbdae4f0f01ba5720c035f275
parsa <hi@parsam.io>
2022-07-18 08:59:14 +0100

    update neovim

--- nvim/lua/user/format.lua
+++ nvim/lua/user/format.lua
@@ -1,6 +1,6 @@
-vim.cmd[[
+vim.cmd([[
 augroup fmt
   autocmd!
-  autocmd BufWritePre * undojoin | Neoformat
+  au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
 augroup END
-]]
+]])
