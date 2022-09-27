02791cf79ea995c0f61e3eb8c22125ec7ed40ff3
parsa <hi@parsam.io>
2022-09-27 23:13:01 +0100

    barely an update, just formatted the file...

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,51 +26,51 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- appearance
-  use("morhetz/gruvbox")
-  use("kyazdani42/nvim-web-devicons")
-  use("nvim-lualine/lualine.nvim")
-  use("akinsho/bufferline.nvim")
-  use("mhinz/vim-startify")
-  use("lukas-reineke/indent-blankline.nvim")
+	-- appearance
+	use("morhetz/gruvbox")
+	use("kyazdani42/nvim-web-devicons")
+	use("nvim-lualine/lualine.nvim")
+	use("akinsho/bufferline.nvim")
+	use("mhinz/vim-startify")
+	use("lukas-reineke/indent-blankline.nvim")
 
-  -- lsp
-  use("neovim/nvim-lspconfig")
-  use("williamboman/nvim-lsp-installer")
+	-- lsp
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
 
-  -- cmp
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-path")
-  use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
+	-- cmp
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-path")
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-  -- file navigating
-  use({
-    "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
-  use("kyazdani42/nvim-tree.lua")
+	-- file navigating
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("kyazdani42/nvim-tree.lua")
 
-  -- utils
-  use("moll/vim-bbye")
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  })
-  use("lewis6991/impatient.nvim")
-  use("windwp/nvim-autopairs")
-  use("windwp/nvim-ts-autotag")
-  use("numToStr/Comment.nvim")
-  use("JoosepAlviste/nvim-ts-context-commentstring")
-  use("lewis6991/gitsigns.nvim")
-  use("sbdchd/neoformat")
+	-- utils
+	use("moll/vim-bbye")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("lewis6991/impatient.nvim")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use("numToStr/Comment.nvim")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("lewis6991/gitsigns.nvim")
+	use("sbdchd/neoformat")
 end)
