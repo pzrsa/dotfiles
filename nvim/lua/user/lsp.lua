e9f072130007fc124b122bc7bc91553082e0082b
parsa <hi@parsam.io>
2022-09-30 17:45:56 +0100

    disable virtual text

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -92,3 +92,25 @@ for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+local config = {
+	-- disable virtual text
+	virtual_text = false,
+	-- show signs
+	signs = {
+		active = signs,
+	},
+	update_in_insert = false,
+	underline = true,
+	severity_sort = false,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+}
+
+vim.diagnostic.config(config)
