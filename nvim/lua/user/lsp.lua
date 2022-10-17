70e09dd3ee98bf1f7757dadeada3ff6eb0b88aac
parsa <hi@parsam.io>
2022-10-17 20:54:19 +0100

    update neovim

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -7,37 +7,6 @@ local lspkind = require("lspkind")
 local saga = require("lspsaga")
 local null_ls = require("null-ls")
 
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(
-    bufnr,
-    "textDocument/formatting",
-    vim.lsp.util.make_formatting_params({}),
-    function(err, res, ctx)
-      if err then
-        local err_msg = type(err) == "string" and err or err.message
-        -- you can modify the log message / level (or ignore it completely)
-        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-        return
-      end
-
-      -- don't apply results if buffer is unloaded or has been modified
-      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-        return
-      end
-
-      if res then
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("silent noautocmd update")
-        end)
-      end
-    end
-  )
-end
-
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
@@ -49,11 +18,11 @@ null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePost", {
+      vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          async_formatting(bufnr)
+          vim.lsp.buf.format({ bufnr = bufnr })
         end,
       })
     end
@@ -108,7 +77,7 @@ lsp_installer.setup({
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-saga.init_lsp_saga()
+saga.init_lsp_saga({})
 
 -- Lsp finder find the symbol definition implement reference
 -- if there is no implement it will hide
@@ -136,7 +105,7 @@ keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 
 -- Outline
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+keymap("n", "<C-m>", "<cmd>LSoutlineToggle<CR>", opts)
 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
