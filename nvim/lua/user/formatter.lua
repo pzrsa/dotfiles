5fcf5ee7b7e63341f288910a925875a8e1102774
parsa <hi@parsam.io>
2022-11-20 21:22:36 +0000

    migrate to using mason for managing lsps/formatters

---
+++ nvim/lua/user/formatter.lua
@@ -0,0 +1,23 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- All formatter configurations are opt-in
+  filetype = {
+    -- Formatter configurations for filetype "lua" go here
+    -- and will be executed in order
+    lua = {
+      -- "formatter.filetypes.lua" defines default configurations for the
+      -- "lua" filetype
+      require("formatter.filetypes.lua").stylua,
+    },
+  }
+}
+
+vim.cmd[[
+augroup FormatAutogroup
+  autocmd!
+  autocmd BufWritePost * FormatWrite
+augroup END
+]]
--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,83 +1,53 @@
-local lsp_installer = require("nvim-lsp-installer")
 local lspconfig = require("lspconfig")
 local cmp = require("cmp")
 local cmpnvimlsp = require("cmp_nvim_lsp")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 local saga = require("lspsaga")
-local null_ls = require("null-ls")
+local mason_lsp = require("mason-lspconfig")
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.goimports,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+require("mason").setup()
+mason_lsp.setup({
+	ensure_installed = {
+		"bashls",
+		"clangd",
+		"cmake",
+		"cssls",
+		"dockerls",
+		"eslint",
+		"gopls",
+		"html",
+		"jsonls",
+		"jdtls",
+		"kotlin_language_server",
+		"sumneko_lua",
+		"pyright",
+		"tailwindcss",
+		"tsserver",
+		"lemminx",
+	},
 })
 
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  completion = {
-    completeopt = "menu,menuone,noinsert",
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = "symbol", -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-    }),
-  },
-})
+local on_attach = function(client, bufnr)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+end
+local capabilities = cmpnvimlsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+for _, server in ipairs(mason_lsp.get_installed_servers()) do
+	lspconfig[server].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+end
 
 require("luasnip.loaders.from_vscode").lazy_load()
-
-lsp_installer.setup({
-  ensure_installed = {
-    "sumneko_lua",
-    "tsserver",
-    "gopls",
-    "eslint",
-    "dockerls",
-    "clangd",
-    "tailwindcss",
-    "prismals",
-    "cssls",
-  },
-})
+saga.init_lsp_saga({})
 
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
-saga.init_lsp_saga({})
 
 -- Lsp finder find the symbol definition implement reference
 -- if there is no implement it will hide
@@ -112,58 +82,72 @@ keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
-local on_attach = function(client, bufnr)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-end
-
-local capabilities = cmpnvimlsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-for _, server in ipairs(lsp_installer.get_installed_servers()) do
-  lspconfig[server.name].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-end
-
 lspconfig.sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 local config = {
-  -- disable virtual text
-  virtual_text = false,
-  -- show signs
-  signs = {
-    active = signs,
-  },
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
+	-- disable virtual text
+	virtual_text = false,
+	-- show signs
+	signs = {
+		active = signs,
+	},
+	update_in_insert = false,
+	underline = true,
+	severity_sort = false,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 }
 
 vim.diagnostic.config(config)
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "path" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+		}),
+	},
+})
