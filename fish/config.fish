f788af52e6028ce9e85806a50074ed4a519836ea
parsa <hi@parsam.io>
2025-02-20 21:48:59 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,8 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 alias c="clear"
 alias vi="nvim"
-fish_vi_key_bindings
+alias ls="eza -l --icons"
+set -g fish_key_bindings fish_vi_key_bindings
 
 source "/opt/homebrew/opt/fzf/shell/key-bindings.fish"
 fzf_key_bindings
@@ -11,3 +12,5 @@ export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
 fnm env --use-on-cd --shell fish | source
+
+bind -M insert \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
