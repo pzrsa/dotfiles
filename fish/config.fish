7677071eff237ac4dfb59c984dfd66e837cf8034
parsa <hi@parsam.io>
2025-04-04 16:58:31 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,10 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 alias c="clear"
 alias vi="nvim"
+alias lg="lazygit"
 alias ls="eza -l --icons"
+alias g="git"
+
 set -g fish_key_bindings fish_vi_key_bindings
 
 source "/opt/homebrew/opt/fzf/shell/key-bindings.fish"
@@ -10,7 +13,13 @@ fzf_key_bindings
 
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+export EDITOR=nvim
 
-fnm env --use-on-cd --shell fish | source
-
-bind -M insert \co 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
