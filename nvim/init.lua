ccc8b741752fd799a28cd6be04126270c919a4f6
parsa <hi@parsam.io>
2025-06-06 00:01:21 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,345 +53,349 @@ vim.cmd([[
 
 -- Autocmds
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- Core functionality
-  { "numToStr/Comment.nvim",  opts = {} },
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
-  },
+	-- Core functionality
+	{ "numToStr/Comment.nvim", opts = {} },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 
-  -- Which-key
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = { preset = "helix" },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = true })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
-  },
+	-- Which-key
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = { preset = "helix" },
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = true })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 
-  -- Fuzzy finder
-  {
-    "nvim-telescope/telescope.nvim",
-    event = "VimEnter",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      },
-      { "nvim-telescope/telescope-ui-select.nvim" },
-      { "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
-    },
-    config = function()
-      require("telescope").setup({
-        pickers = {
-          find_files = { hidden = true },
-        },
-        defaults = {
-          file_ignore_patterns = { "node_modules" },
-          wrap_results = true,
-        },
-      })
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = { hidden = true },
+				},
+				defaults = {
+					file_ignore_patterns = { "node_modules" },
+					wrap_results = true,
+				},
+			})
 
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("ui-select")
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "Telescope diagnostics" })
-      vim.keymap.set("n", "<leader>R", builtin.oldfiles, { desc = "Telescope recent files" })
-      vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Telescope find help" })
-      vim.keymap.set("n", "<leader>k", builtin.keymaps, { desc = "Telescope find keymaps" })
-      vim.keymap.set("n", "<leader>'", builtin.resume, { desc = "Telescope last picker" })
-    end,
-  },
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "Telescope diagnostics" })
+			vim.keymap.set("n", "<leader>R", builtin.oldfiles, { desc = "Telescope recent files" })
+			vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Telescope find help" })
+			vim.keymap.set("n", "<leader>k", builtin.keymaps, { desc = "Telescope find keymaps" })
+			vim.keymap.set("n", "<leader>'", builtin.resume, { desc = "Telescope last picker" })
+		end,
+	},
 
-  -- LSP (updated for Neovim 0.11)
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Configure LSP attachment behavior
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Configure LSP attachment behavior
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
 
-          map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
-          map("gr", require("telescope.builtin").lsp_references, "Goto References")
-          map("gi", require("telescope.builtin").lsp_implementations, "Goto Implementation")
-          map("<leader>s", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
-          map("<leader>r", vim.lsp.buf.rename, "Rename")
-          map("<leader>.", vim.lsp.buf.code_action, "Code Action")
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
-          map("[d", vim.diagnostic.goto_prev, "Go to previous error message")
-          map("]d", vim.diagnostic.goto_next, "Go to next error message")
+					map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+					map("gr", require("telescope.builtin").lsp_references, "Goto References")
+					map("gi", require("telescope.builtin").lsp_implementations, "Goto Implementation")
+					map("<leader>s", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+					map("<leader>r", vim.lsp.buf.rename, "Rename")
+					map("<leader>.", vim.lsp.buf.code_action, "Code Action")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					-- Fix: Wrap diagnostic.jump in anonymous functions
+					map("[d", function()
+						vim.diagnostic.jump({ count = -1 })
+					end, "Go to previous error message")
+					map("]d", function()
+						vim.diagnostic.jump({ count = 1 })
+					end, "Go to next error message")
 
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
 
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-        end,
-      })
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+					end
+				end,
+			})
 
-      -- Configure diagnostic display
-      vim.diagnostic.config({
-        virtual_text = true,
-      })
+			-- Configure diagnostic display
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
 
-      -- Configure LSP servers using the new Neovim 0.11 API
-      -- Lua Language Server
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-            runtime = { version = "LuaJIT" },
-          },
-        },
-      })
+			-- Configure LSP servers using the new Neovim 0.11 API
+			-- Lua Language Server
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+						runtime = { version = "LuaJIT" },
+					},
+				},
+			})
 
-      -- Tailwind CSS
-      vim.lsp.config("tailwindcss", {})
+			vim.lsp.config("tailwindcss", {})
+			vim.lsp.config("eslint", {})
+			vim.lsp.config("prismals", {})
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("tailwindcss")
+			vim.lsp.enable("eslint")
+			vim.lsp.enable("prismals")
+		end,
+	},
+	-- TypeScript tools
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			settings = {
+				complete_function_calls = true,
+				tsserver_file_preferences = {
+					importModuleSpecifierPreference = "relative",
+				},
+			},
+		},
+	},
 
-      -- ESLint
-      vim.lsp.config("eslint", {})
+	-- Formatting
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				jsonc = { "prettier" },
+				markdown = { "prettier" },
+			},
+		},
+	},
 
-      -- Prisma
-      vim.lsp.config("prismals", {})
+	-- Completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				completion = { completeopt = "menu,menuone,noinsert" },
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete({}),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 
-      -- Enable the configured servers
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("tailwindcss")
-      vim.lsp.enable("eslint")
-      vim.lsp.enable("prismals")
-    end,
-  },
+	-- Syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+					"json",
+					"lua",
+					"go",
+					"markdown",
+					"python",
+					"regex",
+					"sql",
+					"tsx",
+				},
+				sync_install = true,
+				auto_install = true,
+				modules = {},
+				ignore_install = {},
+			})
+		end,
+	},
 
-  -- TypeScript tools
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        complete_function_calls = true,
-        tsserver_file_preferences = {
-          importModuleSpecifierPreference = "relative"
-        }
-      },
-    },
-  },
+	-- UI enhancements
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_foreground = "original"
+			vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+			vim.g.gruvbox_material_transparent_background = 1
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
 
-  -- Formatting
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        jsonc = { "prettier" },
-        markdown = { "prettier" },
-      },
-    },
-  },
+	-- File explorer
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		keys = {
+			{
+				"<C-e>",
+				function()
+					require("neo-tree.command").execute({ toggle = true, position = "current", reveal = true })
+				end,
+				desc = "Explorer NeoTree",
+			},
+		},
+		opts = {
+			window = {
+				mappings = { ["l"] = "open" },
+			},
+		},
+	},
 
-  -- Completion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        completion = { completeopt = "menu,menuone,noinsert" },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete({}),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "path" },
-        },
-      })
-    end,
-  },
+	-- Buffer management
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {
+			animation = true,
+			tabpages = true,
+			sidebar_filetypes = {
+				["neo-tree"] = true,
+			},
+		},
+		version = "^1.0.0",
+	},
 
-  -- Syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "html", "css", "javascript", "typescript", "json",
-          "lua", "go", "markdown", "python", "regex", "sql", "tsx"
-        },
-        auto_install = false,
-      })
-    end,
-  },
+	-- Status line
+	{ "bluz71/nvim-linefly" },
 
-  -- UI enhancements
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_foreground = "original"
-      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
+	-- Utilities
+	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+	{ "windwp/nvim-ts-autotag", event = "VeryLazy", opts = {} },
 
-  -- File explorer
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    keys = {
-      {
-        "<C-e>",
-        function()
-          require("neo-tree.command").execute({ toggle = true, position = "current", reveal = true })
-        end,
-        desc = "Explorer NeoTree",
-      },
-    },
-    opts = {
-      window = {
-        mappings = { ["l"] = "open" },
-      },
-    },
-  },
-
-  -- Buffer management
-  {
-    "romgrk/barbar.nvim",
-    dependencies = {
-      "lewis6991/gitsigns.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
-    opts = {
-      animation = true,
-      tabpages = true,
-      sidebar_filetypes = {
-        ["neo-tree"] = true,
-      },
-    },
-    version = "^1.0.0",
-  },
-
-  -- Status line
-  { "bluz71/nvim-linefly" },
-
-  -- Utilities
-  { "windwp/nvim-autopairs",  event = "InsertEnter", config = true },
-  { "windwp/nvim-ts-autotag", event = "VeryLazy",    opts = {} },
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    keys = {
-      { "<leader>P", function() require("persistence").load() end, desc = "Session" },
-    },
-    opts = { options = vim.opt.sessionoptions:get() },
-  },
-
-  -- AI Assistant
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false,
-    opts = {
-      provider = "copilot",
-      providers = {
-        copilot = {
-          model = "claude-sonnet-4",
-        },
-      },
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "echasnovski/mini.pick",
-      "nvim-telescope/telescope.nvim",
-      "hrsh7th/nvim-cmp",
-      "ibhagwan/fzf-lua",
-      "nvim-tree/nvim-web-devicons",
-      "zbirenbaum/copilot.lua",
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+	-- -- AI Assistant
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	lazy = false,
+	-- 	version = false,
+	-- 	opts = {
+	-- 		provider = "copilot",
+	-- 		providers = {
+	-- 			copilot = {
+	-- 				model = "claude-sonnet-4",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	build = "make",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"echasnovski/mini.pick",
+	-- 		"nvim-telescope/telescope.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 		"ibhagwan/fzf-lua",
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 		"zbirenbaum/copilot.lua",
+	-- 		{
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			opts = {
+	-- 				file_types = { "markdown", "Avante" },
+	-- 			},
+	-- 			ft = { "markdown", "Avante" },
+	-- 		},
+	-- 	},
+	-- },
 })
