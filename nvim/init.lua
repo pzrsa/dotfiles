f1d83f0fa1b59a484f3f50bd26adc8086dec6913
parsa <hi@parsam.io>
2025-06-07 01:17:41 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Options
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.number = true
 vim.opt.laststatus = 3
 vim.g.have_nerd_font = true
@@ -70,9 +72,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ "numToStr/Comment.nvim", opts = {} },
+	{ "numToStr/Comment.nvim", event = "VeryLazy", opts = {} },
 	{
 		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
 		opts = {
 			signs = {
 				add = { text = "+" },
@@ -117,16 +120,21 @@ require("lazy").setup({
 					find_files = { hidden = true },
 				},
 				defaults = {
-					file_ignore_patterns = { "node_modules" },
+					file_ignore_patterns = { "node_modules", ".git" },
 					path_display = function(_, path)
 						local tail = require("telescope.utils").path_tail(path)
 						return string.format("%s (%s)", tail, path)
 					end,
+					layout_strategy = "center",
+					sorting_strategy = "ascending",
+					layout_config = {
+						prompt_position = "top", -- search bar at the top
+					},
 				},
 			})
 
-			require("telescope").load_extension("fzf")
-			require("telescope").load_extension("ui-select")
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
@@ -142,6 +150,7 @@ require("lazy").setup({
 
 	{
 		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
 		dependencies = { "saghen/blink.cmp" },
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -180,17 +189,16 @@ require("lazy").setup({
 				},
 			})
 
-			vim.lsp.config("tailwindcss", {})
-			vim.lsp.config("eslint", {})
-			vim.lsp.config("prismals", {})
 			vim.lsp.enable("lua_ls")
 			vim.lsp.enable("tailwindcss")
 			vim.lsp.enable("eslint")
 			vim.lsp.enable("prismals")
+			vim.lsp.enable("astro")
 		end,
 	},
 	{
 		"pmizio/typescript-tools.nvim",
+		event = "VeryLazy",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {
 			settings = {
@@ -204,25 +212,28 @@ require("lazy").setup({
 
 	{
 		"stevearc/conform.nvim",
+		event = "VeryLazy",
 		opts = {
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				jsonc = { "prettier" },
-				markdown = { "prettier" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				jsonc = { "prettierd" },
+				markdown = { "prettierd" },
+				astro = { "prettierd" },
 			},
 		},
 	},
 
 	{
 		"saghen/blink.cmp",
+		event = "VeryLazy",
 		dependencies = { "rafamadriz/friendly-snippets", "Kaiser-Yang/blink-cmp-avante" },
 		version = "1.*",
 		---@module 'blink.cmp'
@@ -246,31 +257,32 @@ require("lazy").setup({
 			completion = { documentation = { auto_show = false } },
 
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "avante" },
-				providers = {
-					avante = {
-						module = "blink-cmp-avante",
-						name = "Avante",
-						opts = {},
-					},
+				default = {
+					"lsp",
+					"path",
+					"snippets",
+					"buffer",
+					-- "avante"
 				},
+				-- providers = {
+				-- 	avante = {
+				-- 		module = "blink-cmp-avante",
+				-- 		name = "Avante",
+				-- 		opts = {},
+				-- 	},
+				-- },
 			},
 			-- Experimental signature help support
 			signature = { enabled = true },
 
-			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-			--
-			-- See the fuzzy documentation for more information
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extend = { "sources.default" },
 	},
 
-	-- Syntax highlighting
 	{
 		"nvim-treesitter/nvim-treesitter",
+		event = "VimEnter",
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
@@ -288,15 +300,20 @@ require("lazy").setup({
 					"sql",
 					"tsx",
 				},
-				sync_install = true,
-				auto_install = true,
 				modules = {},
-				ignore_install = {},
+				autopairs = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+				incremental_selection = {
+					enable = true,
+				},
 			})
 		end,
 	},
 
-	-- UI enhancements
 	{
 		"sainnhe/gruvbox-material",
 		lazy = false,
@@ -310,7 +327,6 @@ require("lazy").setup({
 		end,
 	},
 
-	-- File explorer
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -339,7 +355,6 @@ require("lazy").setup({
 		},
 	},
 
-	-- Buffer management
 	{
 		"romgrk/barbar.nvim",
 		dependencies = {
@@ -359,64 +374,67 @@ require("lazy").setup({
 		version = "^1.0.0",
 	},
 
-	-- Status line
 	{ "bluz71/nvim-linefly" },
-
-	-- Utilities
-	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
-	{ "windwp/nvim-ts-autotag", event = "VeryLazy", opts = {} },
-
-	-- AI Assistant
 	{
-		"yetone/avante.nvim",
+		"windwp/nvim-autopairs",
 		event = "VeryLazy",
-		version = false,
+		config = true,
 		opts = {
-			provider = "copilot",
-			providers = {
-				copilot = {
-					model = "claude-sonnet-4",
-				},
-			},
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-			"ibhagwan/fzf-lua", -- for file_selector provider fzf
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
+			check_ts = true,
 		},
 	},
+	{ "windwp/nvim-ts-autotag", event = "VeryLazy", opts = {} },
+
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	version = false,
+	-- 	opts = {
+	-- 		provider = "copilot",
+	-- 		providers = {
+	-- 			copilot = {
+	-- 				model = "claude-sonnet-4",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- 	build = "make",
+	-- 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+	-- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+	-- 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+	-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+	-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+	-- 		{
+	-- 			-- support for image pasting
+	-- 			"HakonHarnes/img-clip.nvim",
+	-- 			event = "VeryLazy",
+	-- 			opts = {
+	-- 				-- recommended settings
+	-- 				default = {
+	-- 					embed_image_as_base64 = false,
+	-- 					prompt_for_file_name = false,
+	-- 					drag_and_drop = {
+	-- 						insert_mode = true,
+	-- 					},
+	-- 					-- required for Windows users
+	-- 					use_absolute_path = true,
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 		{
+	-- 			-- Make sure to set this up properly if you have lazy=true
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			opts = {
+	-- 				file_types = { "markdown", "Avante" },
+	-- 			},
+	-- 			ft = { "markdown", "Avante" },
+	-- 		},
+	-- 	},
+	-- },
 })
