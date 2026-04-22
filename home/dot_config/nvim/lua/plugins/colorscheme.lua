c134ca8fe072aebd082ce1933650b2586f635a01
Parsa Mesgarha <parsa@yaak.ai>
2026-04-22 23:44:51 +0200

    update config

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,16 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+      vim.g.gruvbox_material_transparent_background = 1
+
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
 }
